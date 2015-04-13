d9c29b9f9febafc91c1a397795dd9073580baf67
Max Norton <maxbnorton@gmail.com>
2015-04-13 16:40:35 -0700

    move post header content into featured media div

--- hhproto2/content.php
+++ hhproto2/content.php
@@ -21,11 +21,39 @@
 				<?php endif; ?>
 				
 			</a>
+
+		    <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+		    
+		    <div class="post-meta">
+			
+				<span class="post-date"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(get_option('date_format')); ?></a></span>
+				
+				<span class="date-sep"> / </span>
 					
+				<span class="post-author"><?php the_author_posts_link(); ?></span>
+						
+				<?php if( is_sticky() && !has_post_thumbnail() ) { ?> 
+				
+					<span class="date-sep"> / </span>
+				
+					<?php _e('Sticky', 'hemingway'); ?>
+				
+				<?php } ?>
+				
+				<?php if ( current_user_can( 'manage_options' ) ) { ?>
+				
+					<span class="date-sep"> / </span>
+								
+					<?php edit_post_link(__('Edit', 'hemingway')); ?>
+				
+				<?php } ?>
+										
+			</div>
+	
 		</div> <!-- /featured-media -->
 			
-	<?php endif; ?>
-	
+	<?php else : ?>
+
     <h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
     
     <div class="post-meta">
@@ -53,6 +81,8 @@
 		<?php } ?>
 								
 	</div>
+
+	<?php endif; ?.
     
 </div> <!-- /post-header -->
 									                                    	    
