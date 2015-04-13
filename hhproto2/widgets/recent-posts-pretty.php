12bf8b2ed4cb6bc6daa7b6c99621c4932a550bf1
Max Norton <maxbnorton@gmail.com>
2015-04-12 20:58:36 -0700

    troubleshoot recent-posts-pretty.php

--- hhproto2/widgets/recent-posts-pretty.php
+++ hhproto2/widgets/recent-posts-pretty.php
@@ -1,5 +1,5 @@
 <?php
-   $recentposts = get_posts('numberposts=12&');
+   $recentposts = get_posts('numberposts=12');
    foreach ($recentposts as $post) :
        setup_postdata($post); ?>
        <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
