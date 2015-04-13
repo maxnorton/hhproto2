1e11ba0435feb1ad1cf6ffa5847216a815156c3b
Max Norton <maxbnorton@gmail.com>
2015-04-12 20:02:21 -0700

    debug recent posts.php

--- hhproto2/widgets/recent-posts.php
+++ hhproto2/widgets/recent-posts.php
@@ -1,6 +1,9 @@
-<?php /* h/t to andersnoren.se, on whose lovecraft theme this widget is based */
+<p>Here's the recent posts widget.</p>
+<?php 
 
-class hhproto2_recent_posts extends WP_Widget {
+//h/t to andersnoren.se, on whose lovecraft theme this widget is based
+
+/* class hhproto2_recent_posts extends WP_Widget {
 
 	function hhproto2_recent_posts() {
 		parent::WP_Widget(false, $name = __('Recent Posts', 'hhproto2'), array('description' => __('Displays recent blog entries.', 'hhproto2') ));	
@@ -151,4 +154,4 @@ class hhproto2_recent_posts extends WP_Widget {
 		<?php
 	}
 }
-register_widget('hhproto2_recent_posts'); ?>
+register_widget('hhproto2_recent_posts'); */ ?>
