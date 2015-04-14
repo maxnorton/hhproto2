c05c420e39395b1ae46cdbbc704b7e89f3a8f7be
Max Norton <maxbnorton@gmail.com>
2015-04-13 18:01:07 -0700

    disable hemingway custom title filter in hhproto2/functions.php

--- hhproto2/functions.php
+++ hhproto2/functions.php
@@ -17,4 +17,31 @@ add_action('widgets_init', 'hhproto2_unregister_default_widgets');
 
 // register theme widgets
 require_once (get_stylesheet_directory() . "/widgets/recent-posts.php");
+
+/* Custom title function
+function hemingway_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'hemingway' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'hemingway_wp_title', 10, 2 );*/
+
+// Disable Hemingway title filter
+remove_filter( 'wp_title', 'hemingway_wp_title');
+
 ?>
